dc5d4c7a4b6eb0ba2aa5122994f94354d6c54941
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-10-27 11:51:16 +0100

    Add view model for ParkedVehicle details

---
+++ Models/VewModels/ParkedVehicleDetailsVewModel.cs
@@ -0,0 +1,41 @@
+﻿using Garage3.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage3.Models.VewModels
+{
+    public class ParkedVehicleDetailsVewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Vehicle Type")]
+        public VehicleTypes VehicleType { get; set; }
+        public Member Member { get; set; }
+       
+        [Display(Name = "Registration number")]
+        public string RegNum { get; set; }
+
+        [ Display(Name = "Vehicle Color")]
+        public string Color { get; set; }
+
+        [ Display(Name = "Make of Vehicle")]
+        public string Make { get; set; }
+
+        [ Display(Name = "Model of Vehicle")]
+        public string Model { get; set; }
+
+        [Display(Name = "Time of Arrival")]
+        public DateTime ArrivalTime { get; set; }
+
+        [Display(Name = "Parking Time")]
+        [DisplayFormat(DataFormatString = "{0:%d}d {0:%h}h {0:%m}m")]
+        public TimeSpan Period { get; set; }
+        
+        [Display(Name = "Parking Spaces")]
+        public ICollection<ParkingSpace> ParkingSpaces { get; set; }
+    }
+}
