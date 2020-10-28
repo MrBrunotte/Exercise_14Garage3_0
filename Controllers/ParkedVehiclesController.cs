60dfefe490ca46b9ad3071504f65628a4eee1772
mrbrunotte <mrbrunotte@gmail.com>
2020-10-28 11:39:42 +0100

    Update

--- Controllers/ParkedVehiclesController.cs
+++ Controllers/ParkedVehiclesController.cs
@@ -30,7 +30,6 @@ namespace Garage3.Controllers
             var model = new VehicleTypeViewModel
             {
                 VehicleList = vehicles,
-                // for the vehicle type:
                 VehicleTypes = await TypeAsync()
             };
             return View(model);
@@ -56,7 +55,7 @@ namespace Garage3.Controllers
                 _context.ParkedVehicle :
                 _context.ParkedVehicle.Where(m => m.RegNum.Contains(viewModel.SearchString));
 
-            vehicles = viewModel.VehicleType == null ?
+            vehicles = viewModel.VehicleTypes == null ?
                 vehicles :
                 vehicles.Where(m => m.VehicleType == viewModel.VehicleTypes);
 
