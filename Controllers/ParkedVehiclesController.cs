0442ca7118c91b3c3fcc0ded82b3b862f69be46f
mrbrunotte <mrbrunotte@gmail.com>
2020-10-28 11:59:55 +0100

    Dropdown view in Index

--- Controllers/ParkedVehiclesController.cs
+++ Controllers/ParkedVehiclesController.cs
@@ -32,6 +32,7 @@ namespace Garage3.Controllers
                 VehicleList = vehicles,
                 VehicleTypes = await TypeAsync()
             };
+            ViewData["VehicleTypeID"] = new SelectList(_context.Set<VehicleTypes>(), "ID", "VehicleType");
             return View(model);
         }
 
