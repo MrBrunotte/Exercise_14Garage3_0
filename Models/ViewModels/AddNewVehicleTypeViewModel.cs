d14e5c46cd2a62f3d8d978b639454006f62932a2
soile@soile.se <soile@soile.se>
2020-10-29 13:34:24 +0100

    AddNewVehicletyp incl methods in the controller

---
+++ Models/ViewModels/AddNewVehicleTypeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage3.Models.ViewModels
+{
+    public class AddNewVehicleTypeViewModel
+    {
+        public int VehicleTypeID { get; set; }
+
+        [Required, StringLength(30, ErrorMessage = "Do not enter more than 30 characters"), Display(Name = "Vehicle Type")]
+        public string VehicleType { get; set; }
+
+        [Required, Display(Name = "Number of required parking spaces")]
+        public int FillsNumberOfSpaces { get; set; }
+    }
+}
